4fd9477325cdc99ab546f4cc6e5713fb089c4c36
JoE11-y <Josephedoh77@gmail.com>
2022-09-21 11:53:14 +0100

    feedback

--- src/App.js
+++ src/App.js
@@ -1,232 +1,316 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import WalletConnect from "@walletconnect/client";
-import QRCodeModal from "algorand-walletconnect-qrcode-modal";
+import React, { useEffect, useState, useCallback } from "react";
+import "./App.css";
+import { algodClient, app_address, myAlgoConnect } from "./utils/constants";
 import algosdk from "algosdk";
 import { formatJsonRpcRequest } from "@json-rpc-tools/utils";
+import WalletConnect from "@walletconnect/client";
+import QRCodeModal from "algorand-walletconnect-qrcode-modal";
 
 const App = () => {
- const [currentAccount, setCurrentAccount] = useState();
- const [globalCount, setGlobalCount] = useState(0);
- const [walletbalance, setwalletbalance] = useState();
- const [connector, setConnector] = useState();
- const [connected, setConnected] = useState(false);
+  const [currentAccount, setCurrentAccount] = useState(null);
+  const [globalCount, setGlobalCount] = useState(0);
+  const [connector, setConnector] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [loading1, setLoading1] = useState(false);
 
- const app_address = 94361442;
+  //============================================= WALLET CONNECT ========================================//
+  const checkIfWalletIsConnected = useCallback(async () => {
+    try {
+      if (!connector) {
+        console.log("No connection");
+        return;
+      } else {
+        console.log("We have connection", connector);
+      }
+      const { accounts } = connector;
 
- const algodToken =
- "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
- const algodServer = "http://localhost";
- const algodPort = 4001;
- const algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
-
-  
- const checkIfWalletIsConnected = async () => {
-  try {
-   if (!connector.connected) {
-    console.log("No connection");
-    return;
-   } else {
-    console.log("We have connection", connector);
-   }
-   const { accounts } = connector;
-
-   if (accounts.length !== 0) {
-    const account = accounts[0];
-    console.log("Found an authorized account:", account);
-    setCurrentAccount(account);
-   } else {
-    setCurrentAccount();
-    console.log("No authorized account found");
-   }
-  } catch (error) {
-   console.log(error);
-  }
- };
-
- const connectWallet = async () => {
-  try {
-   const bridge = "https://bridge.walletconnect.org";
-   const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal });
-   setConnector(connector);
-
-   if (!connector.connected) {
-    await connector.createSession();
-    console.log("Creating new connector session");
-   }
-
-   if (connector.connected) {
-    const { accounts } = connector;
-    const account = accounts[0];
-    setCurrentAccount(account);
-    setConnected(true);
-   }
-
-   connector.on("connect", (error, payload) => {
-    if (error) {
-     throw error;
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        setCurrentAccount(null);
+        console.log("No authorized account found");
+      }
+    } catch (error) {
+      console.log(error);
     }
-    // Get provided accounts
-    const { accounts } = payload.params[0];
-    console.log(
-     "connector.on connect: Connected an account with address:",
-     accounts[0]
-    );
-    setConnector(connector);
-    setConnected(true);
-    setCurrentAccount(accounts[0]);
-   });
+  }, [connector]);
 
-   connector.on("session_update", (error, payload) => {
-    if (error) {
-     throw error;
+  const walletconnectConnect = async () => {
+    try {
+      const bridge = "https://bridge.walletconnect.org";
+      const connector = new WalletConnect({ bridge, qrcodeModal: QRCodeModal });
+      setConnector(connector);
+
+      if (!connector.connected) {
+        await connector.createSession();
+        console.log("Creating new connector session");
+      }
+
+      if (connector.connected) {
+        const { accounts } = connector;
+        const account = accounts[0];
+        setCurrentAccount(account);
+      }
+
+      connector.on("connect", (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        // Get provided accounts
+        const { accounts } = payload.params[0];
+        console.log(
+          "connector.on connect: Connected an account with address:",
+          accounts[0]
+        );
+        setConnector(connector);
+        setCurrentAccount(accounts[0]);
+      });
+
+      connector.on("session_update", (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        // Get updated accounts
+        const { accounts } = payload.params[0];
+        setCurrentAccount(accounts[0]);
+      });
+
+      connector.on("disconnect", (error, payload) => {
+        if (error) {
+          throw error;
+        }
+        setCurrentAccount(null);
+        setConnector(null);
+      });
+    } catch (error) {
+      console.log("something didn't work in creating connector", error);
     }
-    // Get updated accounts
-    const { accounts } = payload.params[0];
-    setCurrentAccount(accounts[0]);
-   });
+  };
 
-   connector.on("disconnect", (error, payload) => {
-    if (error) {
-     throw error;
+  const closeConnection = async () => {
+    connector.killSession();
+    console.log("Killing session for wallet with address: ", currentAccount);
+    setConnector(null);
+  };
+
+  //=====================================================================================//
+
+  const algoWalletConnect = async () => {
+    myAlgoConnect
+      .connect()
+      .then((accounts) => {
+        const _account = accounts[0];
+        setCurrentAccount(_account.address);
+      })
+      .catch((error) => {
+        console.log("Could not connect to MyAlgo wallet");
+        console.error(error);
+      });
+  };
+
+  //=====================================================================================//
+
+  const disconnectWallet = () => {
+    if (connector) {
+      closeConnection();
     }
-    setCurrentAccount();
-    setConnected(false);
-    setConnector();
-   });
-  } catch (error) {
-   console.log("something didn't work in creating connector", error);
-  }
- };
+    setCurrentAccount(null);
+  };
 
- const disconnectWallet = async () => {
-  connector.killSession();
-  console.log("Killing session for wallet with address: ", currentAccount);
-  setCurrentAccount();
-  setConnector();
-  setConnected(false);
- };
-  
- const add = async () => {
-  // construct transaction
-  let sender = currentAccount;
-  let appArgs = [];
-  appArgs.push(new Uint8Array(Buffer.from("Add")));
-  let params = await algodClient.getTransactionParams().do();
-  const txn = algosdk.makeApplicationNoOpTxn(sender, params, app_address, appArgs);
-  let txId = txn.txID().toString();
+  const add = async () => {
+    try {
+      setLoading(true);
+      // construct transaction
+      let sender = currentAccount;
+      let appArgs = [];
+      appArgs.push(new Uint8Array(Buffer.from("Add")));
+      let params = await algodClient.getTransactionParams().do();
+      const txn = algosdk.makeApplicationNoOpTxn(
+        sender,
+        params,
+        app_address,
+        appArgs
+      );
+      let txId = txn.txID().toString();
 
-  // time to sign . . . which we have to do with walletconnect api
-  const txns = [txn]
-  const txnsToSign = txns.map(txn => {
-   const encodedTxn = Buffer.from(algosdk.encodeUnsignedTransaction(txn)).toString("base64");
-   return {
-    txn: encodedTxn,
-   };
-  });
-  const requestParams = [ txnsToSign ];
-  const request = formatJsonRpcRequest("algo_signTxn", requestParams);
-  const result = await connector.sendCustomRequest(request);
-  // have to go on phone and accept the transaction
-  const decodedResult = result.map(element => {
-  return element ? new Uint8Array(Buffer.from(element, "base64")) : null;
-  });
-  // send and await
-  await algodClient.sendRawTransaction(decodedResult).do();
-  await algosdk.waitForConfirmation(algodClient, txId, 2);
-  let transactionResponse = await algodClient.pendingTransactionInformation(txId).do();
-  console.log("Called app-id:",transactionResponse['txn']['txn']['apid']);
-  if (transactionResponse['global-state-delta'] !== undefined ) {
-   console.log("Global State updated:",transactionResponse['global-state-delta']);
-   await getCount();
-  }
+      if (connector) {
+        // time to sign . . . which we have to do with walletconnect api
+        const txns = [txn];
+        const txnsToSign = txns.map((txn) => {
+          const encodedTxn = Buffer.from(
+            algosdk.encodeUnsignedTransaction(txn)
+          ).toString("base64");
+          return {
+            txn: encodedTxn,
+          };
+        });
+        const requestParams = [txnsToSign];
+        const request = formatJsonRpcRequest("algo_signTxn", requestParams);
+        const result = await connector.sendCustomRequest(request);
+        // have to go on phone and accept the transaction
+        const decodedResult = result.map((element) => {
+          return element
+            ? new Uint8Array(Buffer.from(element, "base64"))
+            : null;
+        });
+        // send and await
+        await algodClient.sendRawTransaction(decodedResult).do();
+      } else {
+        // Sign & submit the transaction with algoconnect wallet
+        let signedTxn = await myAlgoConnect.signTransaction(txn.toByte());
+        console.log("Signed transaction with txID: %s", txId);
+        await algodClient.sendRawTransaction(signedTxn.blob).do();
+      }
 
- }
+      await algosdk.waitForConfirmation(algodClient, txId, 2);
+      let transactionResponse = await algodClient
+        .pendingTransactionInformation(txId)
+        .do();
+      console.log("Called app-id:", transactionResponse["txn"]["txn"]["apid"]);
+      if (transactionResponse["global-state-delta"] !== undefined) {
+        console.log(
+          "Global State updated:",
+          transactionResponse["global-state-delta"]
+        );
+        await getCount();
+      }
 
- const deduct = async () => {
-  // construct transaction
-  let sender = currentAccount;
-  let appArgs = [];
-  appArgs.push(new Uint8Array(Buffer.from("Deduct")));
-  let params = await algodClient.getTransactionParams().do();
-  const txn = algosdk.makeApplicationNoOpTxn(sender, params, app_address, appArgs);
-  let txId = txn.txID().toString();
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
 
-  // time to sign . . . which we have to do with walletconnect api
-  const txns = [txn]
-  const txnsToSign = txns.map(txn => {
-   const encodedTxn = Buffer.from(algosdk.encodeUnsignedTransaction(txn)).toString("base64");
-   return {
-    txn: encodedTxn,
-   };
-  });
-  const requestParams = [ txnsToSign ];
-  const request = formatJsonRpcRequest("algo_signTxn", requestParams);
-  const result = await connector.sendCustomRequest(request);
-  // have to go on phone and accept the transaction
-  const decodedResult = result.map(element => {
-  return element ? new Uint8Array(Buffer.from(element, "base64")) : null;
-  });
-  // send and await
-  await algodClient.sendRawTransaction(decodedResult).do();
-  await algosdk.waitForConfirmation(algodClient, txId, 2);
-  let transactionResponse = await algodClient.pendingTransactionInformation(txId).do();
-  console.log("Called app-id:",transactionResponse['txn']['txn']['apid']);
-  if (transactionResponse['global-state-delta'] !== undefined ) {
-   console.log("Global State updated:",transactionResponse['global-state-delta']);
-   await getCount();
-  }
+  const deduct = async () => {
+    try {
+      setLoading1(true);
+      // construct transaction
+      let sender = currentAccount;
+      let appArgs = [];
+      appArgs.push(new Uint8Array(Buffer.from("Deduct")));
+      let params = await algodClient.getTransactionParams().do();
+      const txn = algosdk.makeApplicationNoOpTxn(
+        sender,
+        params,
+        app_address,
+        appArgs
+      );
+      let txId = txn.txID().toString();
 
- }
+      if (connector) {
+        // time to sign . . . which we have to do with walletconnect api
+        const txns = [txn];
+        const txnsToSign = txns.map((txn) => {
+          const encodedTxn = Buffer.from(
+            algosdk.encodeUnsignedTransaction(txn)
+          ).toString("base64");
+          return {
+            txn: encodedTxn,
+          };
+        });
+        const requestParams = [txnsToSign];
+        const request = formatJsonRpcRequest("algo_signTxn", requestParams);
+        const result = await connector.sendCustomRequest(request);
+        // have to go on phone and accept the transaction
+        const decodedResult = result.map((element) => {
+          return element
+            ? new Uint8Array(Buffer.from(element, "base64"))
+            : null;
+        });
+        // send and await
+        await algodClient.sendRawTransaction(decodedResult).do();
+      } else {
+        // Sign & submit the transaction with algoconnect wallet
+        let signedTxn = await myAlgoConnect.signTransaction(txn.toByte());
+        console.log("Signed transaction with txID: %s", txId);
+        await algodClient.sendRawTransaction(signedTxn.blob).do();
+      }
 
- const getCount = async () => {
-  let applicationInfoResponse = await algodClient.getApplicationByID(app_address).do();
-  let globalState = []
-  globalState = applicationInfoResponse['params']['global-state']
-  console.log("count is: ", globalState[0]['value']['uint']);
-  setGlobalCount(globalState[0]['value']['uint']);
- }
+      await algosdk.waitForConfirmation(algodClient, txId, 2);
+      let transactionResponse = await algodClient
+        .pendingTransactionInformation(txId)
+        .do();
+      console.log("Called app-id:", transactionResponse["txn"]["txn"]["apid"]);
+      if (transactionResponse["global-state-delta"] !== undefined) {
+        console.log(
+          "Global State updated:",
+          transactionResponse["global-state-delta"]
+        );
+        await getCount();
+      }
+      setLoading1(false);
+    } catch (error) {
+      console.log(error);
+      setLoading1(false);
+    }
+  };
 
+  const getCount = async () => {
+    let applicationInfoResponse = await algodClient
+      .getApplicationByID(app_address)
+      .do();
+    let globalState = [];
+    globalState = applicationInfoResponse["params"]["global-state"];
+    console.log("count is: ", globalState[0]["value"]["uint"]);
+    setGlobalCount(globalState[0]["value"]["uint"]);
+  };
 
- useEffect(() => {
-  checkIfWalletIsConnected();
-  getCount();
-  console.log('currentAccount:', currentAccount);
- }, [currentAccount])
+  useEffect(() => {
+    checkIfWalletIsConnected();
 
- return (
-  <div className = "mainContainer">
-   <div className = "dataContainer">
-    <div className="header">
-    😉 woohoo!
+    getCount();
+    console.log("currentAccount:", currentAccount);
+  }, [currentAccount, connector, checkIfWalletIsConnected]);
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">😉 woohoo!</div>
+        <div className="bio">Cnu here. Plz connect your wallet!</div>
+        {!currentAccount && (
+          <>
+            <button className="mathButton" onClick={walletconnectConnect}>
+              Connect With WalletConnect
+            </button>
+            <button className="mathButton" onClick={algoWalletConnect}>
+              Connect With AlgoWallet
+            </button>
+          </>
+        )}
+        {currentAccount && (
+          <>
+            <div className="count">{globalCount}</div>
+            <button className="mathButton" onClick={add}>
+              {loading ? (
+                <i
+                  className="fa fa-circle-o-notch fa-spin"
+                  style={{ fontSize: "15px" }}
+                ></i>
+              ) : (
+                "Add"
+              )}
+            </button>
+            <button className="mathButton" onClick={deduct}>
+              {loading1 ? (
+                <i
+                  className="fa fa-circle-o-notch fa-spin"
+                  style={{ fontSize: "15px" }}
+                ></i>
+              ) : (
+                "Deduct"
+              )}
+            </button>
+            <button className="mathButton" onClick={disconnectWallet}>
+              Disconnect Wallet
+            </button>
+          </>
+        )}
+      </div>
     </div>
-    <div className="bio">
-    Cnu here. Plz connect your wallet!
-    </div>
-    {!currentAccount && (
-     <button className="mathButton" onClick={connectWallet}>
-      Connect Wallet
-     </button>
-    )}
-    {currentAccount && (
-    <>
-    <div className = "count">
-     {globalCount}
-    </div>
-    <button className="mathButton" onClick={add}>
-     Add
-    </button>
-    <button className="mathButton" onClick={deduct}>
-     Deduct
-    </button>
-    <button className="mathButton" onClick={disconnectWallet}>
-     Disconnect Wallet
-    </button>
-    </>
-    )}
-   </div>
-  </div>
- );
-}
+  );
+};
 
 export default App;
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+import algosdk from "algosdk";
+import MyAlgoConnect from "@randlabs/myalgo-connect";
+
+const config = {
+  algodToken: "",
+  algodServer: "https://node.testnet.algoexplorerapi.io",
+  algodPort: "",
+};
+
+export const algodClient = new algosdk.Algodv2(
+  config.algodToken,
+  config.algodServer,
+  config.algodPort
+);
+
+export const myAlgoConnect = new MyAlgoConnect();
+
+export const app_address = 94361442;
